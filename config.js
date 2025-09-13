4ce797960cbf80f4fb446a12124786dc0fb6ffab
Gif <g2239104@gmail.com>
2025-09-13 07:26:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,11 @@
 // Daftar nomor admin (nomor WhatsApp dalam format international, contoh: '6281234567890')
 exports.admins = [
-      '6281398809563', // contoh admin
       // tambahkan nomor lain jika perlu
 ];
 
 // Daftar grup yang diizinkan (ID grup WhatsApp, contoh: '12345-67890@g.us')
 exports.allowedGroups = [
-      '120363396075635997@g.us'
       // '12345-67890@g.us',
       // tambahkan ID grup lain jika perlu
 ];
+
